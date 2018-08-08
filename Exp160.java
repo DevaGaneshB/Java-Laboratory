135945fa9b0a5f2e9f668d489bfbc78dfac818d5
DevaGaneshB <40600219+DevaGaneshB@users.noreply.github.com>
2018-08-08 14:22:58 +0530

    Add files via upload

---
+++ Exp160.java
@@ -0,0 +1,17 @@
+//Java Serialization
+		import java.io.*;  
+	
+public class Exp160 {
+		 public static void main(String args[])throws Exception{  
+		  Student s1 =new Student(211,"ravi");  
+		  
+		  FileOutputStream fout=new FileOutputStream("f.txt");  
+		  ObjectOutputStream out=new ObjectOutputStream(fout);  
+		  
+		  out.writeObject(s1);  
+		  out.flush();  
+	  System.out.println("success");  
+		 }  
+	  
+
+}
